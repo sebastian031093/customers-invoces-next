68c82fa84bc34d144a9077402a97e7a3d427b7aa
Sebastian Manco G <sebasmg1393@gmail.com>
2025-09-19 15:57:20 -0500

    fecture finished

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,45 @@
 'use client';
-
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  //2. Update the URL with the search params
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  //1. Capture the user's input}
+
+  /* function handleInpunt(str: string) {
+    console.log(`Searching... ${str}`);
+
+    const params = new URLSearchParams(searchParams);
+
+    if (str) {
+      //Now that you have the query string
+      params.set('query', str);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  } */
+
+  // Inside the Search Component...
+  const handleSearch = useDebouncedCallback(term => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +48,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={event => {
+          handleSearch(event.target.value);
+        }}
+        /* 3. Keeping the URL and input in sync */
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
