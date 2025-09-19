dec2cca15d19d60bb0996c3f0a9bf59d370bfaef
Sebastian Manco G <sebasmg1393@gmail.com>
2025-09-19 14:08:56 -0500

    lecture finished

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import clsx from 'clsx';
 import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -16,7 +16,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathName = usePathname();
+  const pathname = usePathname();
 
   return (
     <>
@@ -26,9 +26,15 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={` ${
+            /*  className={` ${
               pathName === link.name ? 'bg-sky-100' : 'text-blue-600'
-            }     flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3`}
+            }     flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3`} */
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
